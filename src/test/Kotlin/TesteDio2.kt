cd875870a9aa1a6dafa2f51df23be6ab6d62c84f
Neoneo28 <fakihallouche@gmail.com>
2021-08-02 00:07:39 -0300

    Testes da Digital Innovation One

--- src/test/Kotlin/TesteDio2.kt
+++ src/test/Kotlin/TesteDio2.kt
@@ -1,17 +1,36 @@
-//Desafio
+// Média 1
 
-// Você receberá dois valores inteiros. Faça a leitura e em seguida calcule o produto entre estes dois valores. Atribua esta operação à variável PROD, mostrando esta de acordo com a mensagem de saída esperada (exemplo abaixo).
+/*
+Leia 2 valores de ponto flutuante de dupla precisão A e B, que correspondem a
+2 notas de um aluno. A seguir, calcule a média do aluno, sabendo que a nota A
+tem peso 3.5 e a nota B tem peso 7.5 (A soma dos pesos portanto é 11). Assuma
+que cada nota pode ir de 0 até 10.0, sempre com uma casa decimal.
+- Entrada
+O arquivo de entrada contém 2 valores com uma casa decimal cada um.
+- Saída
+Calcule e imprima a variável MEDIA conforme exemplo abaixo, com 5 dígitos após
+o ponto decimal e com um espaço em branco antes e depois da igualdade. Utilize
+variáveis de dupla precisão (double) e como todos os problemas, não esqueça de
+imprimir o fim de linha após o resultado, caso contrário, você receberá
+"Presentation Error".
+*/
 
-//Entrada A entrada contém 2 valores inteiros.
-//Saída
-
-//Exiba a variável PROD conforme exemplo abaixo, tendo obrigatoriamente um espaço em branco antes e depois da igualdade.
-
+import java.util.Scanner
 
 fun main(args: Array<String>) {
+    var reader = Scanner(System.`in`)
 
-    val a = readLine()!!.toInt()
-    val b = readLine()!!.toInt()
+    var a = reader.nextDouble()
+    var b = reader.nextDouble()
 
-    println("PROD = ${a * b} ")  //insira as variaveis corretamente
+    var weightA = 3.5
+    var weightB = 7.5
+
+    var totalWeight = weightA + weightB
+
+    var m = (a *  weightA  + b * weightB) / totalWeight
+
+    var r = "%.5f".format(m)
+
+    println("MEDIA = ${r}")
 }
